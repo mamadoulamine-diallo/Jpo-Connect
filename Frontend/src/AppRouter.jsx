e4fc816fcdb03113b8117c9dbbdef2d2a503f052
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-06-09 16:19:31 +0200

    install composer for PhpMailer / php test_mail.php

--- Frontend/src/AppRouter.jsx
+++ Frontend/src/AppRouter.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import App from "./App";
+import JPOPage from "./pages/JPOPage"; 
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/app" element={<App />} />
+        <Route path="/jpo" element={<JPOPage />} />
       </Routes>
     </Router>
   );
